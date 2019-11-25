902cd047a3522acf74f269dceae809615f49d3a5
Daniel Garza <danielriosgarza@gmail.com>
2019-11-25 10:50:56 +0100

    updated tasks

---
+++ scripts/get_patric_genomes.sh
@@ -0,0 +1,6 @@
+wget ftp://ftp.patricbrc.org/genomes/469603.3/469603.3.fna
+
+wget ftp://ftp.patricbrc.org/genomes/100225.4/100225.4.fna
+
+wget ftp://ftp.patricbrc.org/genomes/1002691.3/1002691.3.fna
+
