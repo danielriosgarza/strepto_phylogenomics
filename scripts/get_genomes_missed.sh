f9d73c9768c907f56df8e51ee44693fbd313d65b
meikero <meike.rohrbach@yahoo.com>
2019-12-18 11:06:58 +0100

    missing strepto genomes

--- scripts/get_genomes_missed.sh
+++ scripts/get_genomes_missed.sh
@@ -1,3 +1,3 @@
 wget ftp://ftp.patricbrc.org/genomes/2491052.3/2491052.3.fna -O /home/meiker/git/genomes/streptococcus_11895.fna
 wget ftp://ftp.patricbrc.org/genomes/2490633.3/2490633.3.fna -O /home/meiker/git/genomes/streptococcus_11939.fna
-wget ftp://ftp.patricbrc.org/genomes/1494462.5/1494462.5.fna -O /home/meiker/git/genomes/lactococcus_00188.fna
+
