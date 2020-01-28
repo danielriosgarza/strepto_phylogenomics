23128ebc21bd7ffdd3f99ebcf6c5e21649433665
meikero <meike.rohrbach@yahoo.com>
2020-01-28 10:26:06 +0100

    28/01/20

--- scripts/bash_scripts/blat/280120_blat_run.sh
+++ scripts/bash_scripts/blat/280120_blat_run.sh
@@ -1,20 +1,20 @@
-/home/meiker/software/blat/blat -prot /home/meiker/orthomcl/filteredFasta/goodProteins.fasta /home/meiker/git/data/prokka_annotation/streptococcus_01571/streptococcus_01571.faa /home/meiker/blat/streptococcus_01571.blast -out=blast -threads=8
+/home/meiker/software/blat/blat -prot /home/meiker/orthomcl/filteredFasta/goodProteins.fasta /home/meiker/git/data/prokka_annotation/streptococcus_01571/streptococcus_01571.faa /home/meiker/blat/streptococcus_01571.blast -out=blast 
 
-/home/meiker/software/blat/blat -prot /home/meiker/orthomcl/filteredFasta/goodProteins.fasta /home/meiker/git/data/prokka_annotation/streptococcus_01652/streptococcus_01652.faa /home/meiker/blat/streptococcus_01652.blast -out=blast -threads=8
+/home/meiker/software/blat/blat -prot /home/meiker/orthomcl/filteredFasta/goodProteins.fasta /home/meiker/git/data/prokka_annotation/streptococcus_01652/streptococcus_01652.faa /home/meiker/blat/streptococcus_01652.blast -out=blast 
 
-/home/meiker/software/blat/blat -prot /home/meiker/orthomcl/filteredFasta/goodProteins.fasta /home/meiker/git/data/prokka_annotation/streptococcus_08774/streptococcus_08774.faa /home/meiker/blat/streptococcus_08774.blast -out=blast -threads=8
+/home/meiker/software/blat/blat -prot /home/meiker/orthomcl/filteredFasta/goodProteins.fasta /home/meiker/git/data/prokka_annotation/streptococcus_08774/streptococcus_08774.faa /home/meiker/blat/streptococcus_08774.blast -out=blast 
 
-/home/meiker/software/blat/blat -prot /home/meiker/orthomcl/filteredFasta/goodProteins.fasta /home/meiker/git/data/prokka_annotation/streptococcus_04699/streptococcus_04699.faa /home/meiker/blat/streptococcus_04699.blast -out=blast -threads=8
+/home/meiker/software/blat/blat -prot /home/meiker/orthomcl/filteredFasta/goodProteins.fasta /home/meiker/git/data/prokka_annotation/streptococcus_04699/streptococcus_04699.faa /home/meiker/blat/streptococcus_04699.blast -out=blast 
 
-/home/meiker/software/blat/blat -prot /home/meiker/orthomcl/filteredFasta/goodProteins.fasta /home/meiker/git/data/prokka_annotation/streptococcus_11890/streptococcus_11890.faa /home/meiker/blat/streptococcus_11890.blast -out=blast -threads=8
+/home/meiker/software/blat/blat -prot /home/meiker/orthomcl/filteredFasta/goodProteins.fasta /home/meiker/git/data/prokka_annotation/streptococcus_11890/streptococcus_11890.faa /home/meiker/blat/streptococcus_11890.blast -out=blast 
 
-/home/meiker/software/blat/blat -prot /home/meiker/orthomcl/filteredFasta/goodProteins.fasta /home/meiker/git/data/prokka_annotation/streptococcus_09801/streptococcus_09801.faa /home/meiker/blat/streptococcus_09801.blast -out=blast -threads=8
+/home/meiker/software/blat/blat -prot /home/meiker/orthomcl/filteredFasta/goodProteins.fasta /home/meiker/git/data/prokka_annotation/streptococcus_09801/streptococcus_09801.faa /home/meiker/blat/streptococcus_09801.blast -out=blast 
 
-/home/meiker/software/blat/blat -prot /home/meiker/orthomcl/filteredFasta/goodProteins.fasta /home/meiker/git/data/prokka_annotation/streptococcus_04196/streptococcus_04196.faa /home/meiker/blat/streptococcus_04196.blast -out=blast -threads=8
+/home/meiker/software/blat/blat -prot /home/meiker/orthomcl/filteredFasta/goodProteins.fasta /home/meiker/git/data/prokka_annotation/streptococcus_04196/streptococcus_04196.faa /home/meiker/blat/streptococcus_04196.blast -out=blast 
 
-/home/meiker/software/blat/blat -prot /home/meiker/orthomcl/filteredFasta/goodProteins.fasta /home/meiker/git/data/prokka_annotation/streptococcus_02702/streptococcus_02702.faa /home/meiker/blat/streptococcus_02702.blast -out=blast -threads=8
+/home/meiker/software/blat/blat -prot /home/meiker/orthomcl/filteredFasta/goodProteins.fasta /home/meiker/git/data/prokka_annotation/streptococcus_02702/streptococcus_02702.faa /home/meiker/blat/streptococcus_02702.blast -out=blast 
 
-/home/meiker/software/blat/blat -prot /home/meiker/orthomcl/filteredFasta/goodProteins.fasta /home/meiker/git/data/prokka_annotation/lactococcus_00046/lactococcus_00046.faa /home/meiker/blat/lactococcus_00046.blast -out=blast -threads=8
+/home/meiker/software/blat/blat -prot /home/meiker/orthomcl/filteredFasta/goodProteins.fasta /home/meiker/git/data/prokka_annotation/lactococcus_00046/lactococcus_00046.faa /home/meiker/blat/lactococcus_00046.blast -out=blast 
 
-/home/meiker/software/blat/blat -prot /home/meiker/orthomcl/filteredFasta/goodProteins.fasta /home/meiker/git/data/prokka_annotation/streptococcus_07749/streptococcus_07749.faa /home/meiker/blat/streptococcus_07749.blast -out=blast -threads=8
+/home/meiker/software/blat/blat -prot /home/meiker/orthomcl/filteredFasta/goodProteins.fasta /home/meiker/git/data/prokka_annotation/streptococcus_07749/streptococcus_07749.faa /home/meiker/blat/streptococcus_07749.blast -out=blast 
 
