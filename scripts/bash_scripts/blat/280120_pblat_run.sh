e2d22807511bf7dec4a138eed27838258658fb67
meikero <meike.rohrbach@yahoo.com>
2020-01-28 14:45:06 +0100

    28/01/20 pblat test

---
+++ scripts/bash_scripts/blat/280120_pblat_run.sh
@@ -0,0 +1,20 @@
+/home/meiker/software/icebert-pblat-652d3b3/pblat -prot -threads=8 -out=blast /home/meiker/orthomcl/filteredFasta/goodProteins.fasta /home/meiker/git/data/prokka_annotation/streptococcus_01571/streptococcus_01571.faa /home/meiker/pblat/streptococcus_01571.blast
+
+/home/meiker/software/icebert-pblat-652d3b3/pblat -prot -threads=8 -out=blast /home/meiker/orthomcl/filteredFasta/goodProteins.fasta /home/meiker/git/data/prokka_annotation/streptococcus_01652/streptococcus_01652.faa /home/meiker/pblat/streptococcus_01652.blast
+
+/home/meiker/software/icebert-pblat-652d3b3/pblat -prot -threads=8 -out=blast /home/meiker/orthomcl/filteredFasta/goodProteins.fasta /home/meiker/git/data/prokka_annotation/streptococcus_08774/streptococcus_08774.faa /home/meiker/pblat/streptococcus_08774.blast
+
+/home/meiker/software/icebert-pblat-652d3b3/pblat -prot -threads=8 -out=blast /home/meiker/orthomcl/filteredFasta/goodProteins.fasta /home/meiker/git/data/prokka_annotation/streptococcus_04699/streptococcus_04699.faa /home/meiker/pblat/streptococcus_04699.blast
+
+/home/meiker/software/icebert-pblat-652d3b3/pblat -prot -threads=8 -out=blast /home/meiker/orthomcl/filteredFasta/goodProteins.fasta /home/meiker/git/data/prokka_annotation/streptococcus_11890/streptococcus_11890.faa /home/meiker/pblat/streptococcus_11890.blast
+
+/home/meiker/software/icebert-pblat-652d3b3/pblat -prot -threads=8 -out=blast /home/meiker/orthomcl/filteredFasta/goodProteins.fasta /home/meiker/git/data/prokka_annotation/streptococcus_09801/streptococcus_09801.faa /home/meiker/pblat/streptococcus_09801.blast
+
+/home/meiker/software/icebert-pblat-652d3b3/pblat -prot -threads=8 -out=blast /home/meiker/orthomcl/filteredFasta/goodProteins.fasta /home/meiker/git/data/prokka_annotation/streptococcus_04196/streptococcus_04196.faa /home/meiker/pblat/streptococcus_04196.blast
+
+/home/meiker/software/icebert-pblat-652d3b3/pblat -prot -threads=8 -out=blast /home/meiker/orthomcl/filteredFasta/goodProteins.fasta /home/meiker/git/data/prokka_annotation/streptococcus_02702/streptococcus_02702.faa /home/meiker/pblat/streptococcus_02702.blast
+
+/home/meiker/software/icebert-pblat-652d3b3/pblat -prot -threads=8 -out=blast /home/meiker/orthomcl/filteredFasta/goodProteins.fasta /home/meiker/git/data/prokka_annotation/lactococcus_00046/lactococcus_00046.faa /home/meiker/pblat/lactococcus_00046.blast
+
+/home/meiker/software/icebert-pblat-652d3b3/pblat -prot -threads=8 -out=blast /home/meiker/orthomcl/filteredFasta/goodProteins.fasta /home/meiker/git/data/prokka_annotation/streptococcus_07749/streptococcus_07749.faa /home/meiker/pblat/streptococcus_07749.blast
+
