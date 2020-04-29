88da9b1350cf42e6b347a4497d146ee2de10b9b8
meikero <meike.rohrbach@yahoo.de>
2020-04-29 11:00:09 +0200

    29/04/20

---
+++ scripts/search_added_strain.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 29 10:54:46 2020
+
+@author: meike
+"""
+
+a = '/home/meiker/phylo_tree/roary/29042020_concat_alignments.fa'
+b = '/home/meiker/phylo_tree/iqtree/reduced_alignments/09032020_reduced_concat_alignments.fa'
+
+l = []
+with open(a) as f:
+    with open(b) as f2:
+        for line in f:
+            if line.startswith('>'):
+                l.append(line)
+        for line in f2:
+            if line.startswith('>'):
+                if line not in l:
+                    print(line)
