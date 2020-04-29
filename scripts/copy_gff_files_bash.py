966436fcf3ba63bd75ccb1a164cc706e7046f486
meikero <meike.rohrbach@yahoo.de>
2020-04-29 10:33:30 +0200

    29/04/20

---
+++ scripts/copy_gff_files_bash.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 29 10:23:00 2020
+
+@author: meike
+"""
+
+
+'''
+Copy chosen gff files to roary directory
+'''
+import os
+from pathlib import Path
+
+path = os.getcwd()
+p = Path(path)
+
+ids = []
+with open('/home/meiker/phylo_tree/roary/29042020_reduced_concat_alignment.fa') as f:
+    for line in f:
+        if line.startswith('>'):
+            id_ = line.strip()[1::]
+            ids.append(id_)
+
+with open(os.path.join(p.partents[0], 'scripts', 'bash_scripts', 'move_gffs.sh'), 'w') as f:
+    for i in ids:
+        f.write('cp /home/git/data/prokka_annotation/' + i + '/' + i + '.gff /home/meiker/roary/gff_files\n')
